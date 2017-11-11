d05bf6a254d0eaa3cc0cf8f81f7369db75edf853
Sefaice <fanruichao1@live.com>
2017-11-11 09:31:28 +0800

    test twice

---
+++ random/main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <qpushbutton.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a( argc, argv );
+
+        QPushButton hello( "Hello world!", 0 );
+        hello.resize( 100, 30 );
+
+
+        hello.show();
+        return a.exec();
+}
